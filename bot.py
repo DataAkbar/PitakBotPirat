3deb259ba8a5c1c8d73a0367b9e594e53d916d1d
AkbarAli <88106359+DataAkbar@users.noreply.github.com>
2021-12-04 21:46:09 +0500

    Botga ozgarishlar kiritildi
    Viloyatlar qoshildi
    ....

--- bot.py
+++ bot.py
@@ -18,28 +18,173 @@ async def send_welcome(message: types.Message):
 
 """ --- Birinchi Haydovchi kerak --- """
 @dp.message_handler(Text(equals=["Haydovchi kerak"]))
-async def accounts(message: types.Message):
+async def haydovchi(message: types.Message):
     await message.answer(f"Siz qaysi viloyatta turibsiz?", reply_markup=Haydovchi_kerak)
 
-# /// Toshkentning shaxarlari
+# """ Toshkentning shaxarlari """
 @dp.message_handler(Text(equals="Toshkent"))
-async def accounts(message: types.Message):
-    await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=toshkent)
+async def toshkents(message: types.Message):
+    await message.answer(f"Siz qaysi tumanda turibsiz?\nTumanlardan birini tanlang", reply_markup=toshkent)
+
+# --- Yunusobod
+@dp.message_handler(Text(equals="Yunusobod"))
+async def yunsobots(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Yashnobod
+@dp.message_handler(Text(equals="Yashnobod"))
+async def yashnaobods(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Yakkasaroy
+@dp.message_handler(Text(equals="Yakkasaroy"))
+async def yakkasaroys(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Shayxontohur
+@dp.message_handler(Text(equals="Shayxontohur"))
+async def shoyxontohurs(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Chilonzor
+@dp.message_handler(Text(equals="Chilonzor"))
+async def chilonzors(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Olmazor
+@dp.message_handler(Text(equals="Olmazor"))
+async def olmazors(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Sergeli 
+@dp.message_handler(Text(equals="Sergeli"))
+async def sergeli(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Mirobod
+@dp.message_handler(Text(equals="Mirobod"))
+async def mirobod(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Mirzo Ulug'bek
+@dp.message_handler(Text(equals="Mirzo Ulug'bek"))
+async def mirzo_ulugbek(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Bektemir
+@dp.message_handler(Text(equals="Bektemir"))
+async def bektemirs(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Uchtepa
+@dp.message_handler(Text(equals="Uchtepa"))
+async def uchtepas(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\Viloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Qibray
+@dp.message_handler(Text(equals="Qibray"))
+async def qibrays(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Piskent
+@dp.message_handler(Text(equals="Piskent"))
+async def piskents(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Parkent
+@dp.message_handler(Text(equals="Parkent"))
+async def parkents(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Bo'ka
+@dp.message_handler(Text(equals="Bo'ka"))
+async def boka(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Bekobod shahri
+@dp.message_handler(Text(equals="Bekobod shahri"))
+async def bekobod_sh(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# --- Angren shahri
+@dp.message_handler(Text(equals="Angren shahri"))
+async def angren(message: types.Message):
+    await message.answer(f"Siz qaysi viloyatga borishingiz kerak?\nViloyatlardan birini tanlang", reply_markup=t_viloyatlar)
+
+# \\\ Surxondaryo ///
+@dp.message_handler(Text(equals="Surxondaryo"))
+async def surxondaryo(message: types.Message):
+    await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=surxondaryo)
+
+# --- Buxoro shaxarlari
+@dp.message_handler(Text(equals="Buxoro"))
+async def buxoras(message: types.Message):
+    await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=buxaro)
+
+# --- Xorazm
+@dp.message_handler(Text(equals="Xorazm"))
+async def xorazms(message: types.Message):
+    await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=xorazm)
+
+# --- Andijon 
+@dp.message_handler(Text(equals="Andijon"))
+async def andijans(message: types.Message):
+    await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=andijon)
+
+# --- Farg'ona
+@dp.message_handler(Text(equals="Farg'ona"))
+async def fargonas(message: types.Message):
+    await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=fargona)
+
+# --- Jizzax
+@dp.message_handler(Text(equals="Jizzax"))
+async def jizzahs(message: types.Message):
+    await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=jizzax)
+
+# --- Qoshqidaryo
+@dp.message_handler(Text(equals="Qoshqidaryo"))
+async def qoshqadaryos(message: types.Message):
+    await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=qoshqadaryo)
+
+# --- Namangan 
+@dp.message_handler(Text(equals="Namangan"))
+async def namangans(message: types.Message):
+    await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=namangan)
+
+# --- Navoi
+@dp.message_handler(Text(equals="Navoi"))
+async def navois(message: types.Message):
+    await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=navoi)
+
+# --- Sirdaryo
+@dp.message_handler(Text(equals="Sirdaryo"))
+async def sirdaros(message: types.Message):
+    pass
+    # await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=)
+
+# --- Samarqand
+@dp.message_handler(Text(equals="Samarqand"))
+async def samarkans(message: types.Message):
+    pass
+    # await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=samarqand)
+
+# --- Qoraqalpoqistan
+@dp.message_handler(Text(equals="Qoraqalpoqistan"))
+async def qalpagistans(message: types.Message):
+    pass
+    # await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=qoraqalpoqistan)
 
 # Orqaga qaytish
 @dp.message_handler(Text(equals=["Orqaga"]))
 async def accounts(message: types.Message):
-    await message.answer(f"Siz qaysi viloyatta turibsiz?", reply_markup=Haydovchi_kerak)
+    await message.answer(f"Tugmalardan ozingizga kerakini tanlang", reply_markup=Haydovchi_kerak)
 
 # Menuga qaytish 
 @dp.message_handler(Text(equals=["Bosh sahifa"]))
 async def accounts(message: types.Message):
-    await message.answer(f"Siz qaysi viloyatta turibsiz?", reply_markup=menu)
+    await message.answer(f"Tugmalardan ozingizga kerakini tanlang", reply_markup=menu)
 
-# \\\ Surxondaryo ///
-@dp.message_handler(Text(equals="Surxondaryo"))
-async def accounts(message: types.Message):
-    await message.answer(f"Siz qaysi tumanda turibsiz?", reply_markup=surxondaryo)
+
 
 """ --- Men haydovchiman --- """
 @dp.message_handler(Text(equals=["Men haydovchiman"]))
@@ -67,7 +212,7 @@ async def contacts(message: types.Message):
 
 @dp.message_handler(Text(equals=["🔙 Orqaga"]))
 async def back(message: types.Message):
-    await message.answer("Bosh menyu", reply_markup=menu)
+    await message.answer("Tugmalardan ozingizga kerakini tanlang", reply_markup=menu)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
--- buttum.py
+++ buttum.py
@@ -16,7 +16,7 @@ Haydovchi_kerak = ReplyKeyboardMarkup(
         [KeyboardButton(text="Xorazm"), KeyboardButton(text="Buxoro")],
         [KeyboardButton(text="Andijon"), KeyboardButton(text="Farg'ona")],
         [KeyboardButton(text="Jizzax"), KeyboardButton(text="Qoshqidaryo")],
-        [KeyboardButton(text="Namamngan"), KeyboardButton(text="Navoi")],
+        [KeyboardButton(text="Namangan"), KeyboardButton(text="Navoi")],
         [KeyboardButton(text="Sirdaryo"), KeyboardButton(text="Samarqand")],
         [KeyboardButton(text="Qoraqalpoqiston")],
         [KeyboardButton(text="Bosh sahifa", reply_markup=menu)],
@@ -25,10 +25,27 @@ Haydovchi_kerak = ReplyKeyboardMarkup(
     # one_time_keyboard=True,
 )
 
+# --- Toshkent ichidagi tumanlar uchun
+t_viloyatlar = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Toshkent"), KeyboardButton(text="Surxondaryo")],
+        [KeyboardButton(text="Xorazm"), KeyboardButton(text="Buxoro")],
+        [KeyboardButton(text="Andijon"), KeyboardButton(text="Farg'ona")],
+        [KeyboardButton(text="Jizzax"), KeyboardButton(text="Qoshqidaryo")],
+        [KeyboardButton(text="Namangan"), KeyboardButton(text="Navoi")],
+        [KeyboardButton(text="Sirdaryo"), KeyboardButton(text="Samarqand")],
+        [KeyboardButton(text="Qoraqalpoqiston")],
+        [KeyboardButton(text="Bosh sahifa")],
+    ],
+    resize_keyboard=True,
+    # one_time_keyboard=True,
+)  
+
+
 # -- \\\ Toshkentni ichidagi shaharlar ///
 toshkent = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="Yunusobod"), KeyboardButton(text="Yshnobod")],
+        [KeyboardButton(text="Yunusobod"), KeyboardButton(text="Yashnobod")],
         [KeyboardButton(text="Yakkasaroy"), KeyboardButton(text="Shayxontohur")],
         [KeyboardButton(text="Chilonzor"), KeyboardButton(text="Olmazor")],
         [KeyboardButton(text="Sergeli"), KeyboardButton(text="Mirobod")],
@@ -43,7 +60,22 @@ toshkent = ReplyKeyboardMarkup(
     # one_time_keyboard=True,
 )
 
-# \\\ Surxondaryo ///
+# --- Xorazm shaxarlar
+xorazm = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Urganch"), KeyboardButton(text="Bogʻot")],
+        [KeyboardButton(text="Gurlan"), KeyboardButton(text="Xonqa")],
+        [KeyboardButton(text="Hazorasp"), KeyboardButton(text="Xiva")],
+        [KeyboardButton(text="Qoʻshkoʻpir"), KeyboardButton(text="Shovot")],
+        [KeyboardButton(text="Qorovul (qishloq)"), KeyboardButton(text="Yangiariq")],
+        [KeyboardButton(text="Yangibozor (Yangibozor tumani)"), KeyboardButton(text="Pitnak")],
+        [KeyboardButton(text="Bosh sahifa"), KeyboardButton(text="Orqaga")],
+    ],
+    resize_keyboard=True,
+    # one_time_keyboard=True,
+)
+
+# \\\ Surxondaryo ichidagi shaxar ///
 surxondaryo = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="Angor"), KeyboardButton(text="Termiz sh")],
@@ -60,24 +92,8 @@ surxondaryo = ReplyKeyboardMarkup(
     # one_time_keyboard=True,
 )
 
-
-""" ---- MEn Haydovchiman niki ---- """
-men_haydovchiman = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="Andijon-Toshkent-Andijon"), KeyboardButton(text="Farg'ona-Toshkent-Farg'ona")],
-        [KeyboardButton(text="Buxoro-Toshkent-Buxoro"), KeyboardButton(text="Jizzax-Toshkent-Jizzax")],
-        [KeyboardButton(text="Namangan-Toshkent-Namangan"), KeyboardButton(text="Navoi-Toshkent-Navoi")],
-        [KeyboardButton(text="Qashqidaryo-Toshkent-Qashqidaryo"), KeyboardButton(text="Qoraqalpoqiston-Toshkent-Qoraqalpaqiston")],
-        [KeyboardButton(text="Samarqand-Toshkent-Samarqand"), KeyboardButton(text="Sirdayo-Toshkent-Sirdaryo")],
-        [KeyboardButton(text="Surxondaryo-Toshkent-Surxondaryo"), KeyboardButton(text="Xorazm-Toshkent-Xorazm")],
-    
-    ],
-    resize_keyboard=True,
-    # one_time_keyboard=True,
-)
-
-
-andijon=ReplyKeyboardMarkup(
+# --- Andijondagi shaxarlar
+andijon = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="Ulug'nor"),KeyboardButton(text="Oltinko'l")],
         [KeyboardButton(text="Paxtaobod"),KeyboardButton(text="shaxrixon")],
@@ -86,6 +102,112 @@ andijon=ReplyKeyboardMarkup(
         [KeyboardButton(text="Izbosgan"),KeyboardButton(text="Andijon shahri")],
         [KeyboardButton(text="Xonaobod shahri"),KeyboardButton(text="Andijon")],
         [KeyboardButton(text="Asaka"),KeyboardButton(text="Baliqchi")],
-        [KeyboardButton(text="Bo'ston"),KeyboardButton(text="Jalaquduq")],        
+        [KeyboardButton(text="Bo'ston"),KeyboardButton(text="Jalaquduq")],
+        [KeyboardButton(text="Bosh sahifa"), KeyboardButton(text="Orqaga")],        
     ]
+)
+
+# --- Buxorodagi shaharlar
+buxaro = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Buxoro"),KeyboardButton(text="G'ijdivon")],
+        [KeyboardButton(text="Qorako'l"),KeyboardButton(text="Kogon")],
+        [KeyboardButton(text="Jondor"),KeyboardButton(text="Olot")],
+        [KeyboardButton(text="Peshku"),KeyboardButton(text="Qorovulbozor")],
+        [KeyboardButton(text="Romiton"),KeyboardButton(text="Shofirkon")],
+        [KeyboardButton(text="Vobkent"),KeyboardButton(text="Kogon shahri")],
+        [KeyboardButton(text="Buxaro shaxri")],
+        [KeyboardButton(text="Bosh sahifa"), KeyboardButton(text="Orqaga")],
+    ],
+    resize_keyboard=True,
+    # one_time_keyboard=True,
+)
+
+# --- Farg'onadagi shaharlar
+fargona = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Uchko'pir"),KeyboardButton(text="Yozyovon"),KeyboardButton(text="Qo'qon shahar")],
+        [KeyboardButton(text="Qo'shtepa"),KeyboardButton(text="Dang'ara"),KeyboardButton(text="Beshariq")],
+        [KeyboardButton(text="Toshloq"),KeyboardButton(text="Buvayda"),KeyboardButton(text="Furqat")],
+        [KeyboardButton(text="Bog'dod"),KeyboardButton(text="Marg'ilon shahar"),KeyboardButton(text="Quva")],
+        [KeyboardButton(text="Rishton"),KeyboardButton(text="Quvasoy shahar"),KeyboardButton(text="So'x")],
+        [KeyboardButton(text="Farg'ona shahri"),KeyboardButton(text="Farg'ona"),KeyboardButton(text="Oltiariq")],
+        [KeyboardButton(text="O'zbekiston")],
+        [KeyboardButton(text="Bosh sahifa"), KeyboardButton(text="Orqaga")],
+    ],
+    resize_keyboard=True,
+    # one_time_keyboard=True,
+)
+
+# --- Jizzaxdagi shaxarlar
+jizzax = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Do'stlik"),KeyboardButton(text="G'allaorol"),KeyboardButton(text="Forish")],
+        [KeyboardButton(text="Baxmal"),KeyboardButton(text="Arnasoy"),KeyboardButton(text="Dashobad shaxri")],
+        [KeyboardButton(text="Zafarobod"),KeyboardButton(text="Yangiobod"),KeyboardButton(text="Paxtakor")],
+        [KeyboardButton(text="Zomin"),KeyboardButton(text="Zomi shahri"),KeyboardButton(text="Mirzacho'l")],
+        [KeyboardButton(text="Jizzax"),KeyboardButton(text="Jizzax shahri")],
+        [KeyboardButton(text="Bosh sahifa"), KeyboardButton(text="Orqaga")],
+    ],
+    resize_keyboard=True,
+    # one_time_keyboard=True,
+)
+
+# --- Namangandagi shaharlar
+namangan = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Namangan shahri"),KeyboardButton(text="Chartoq"),KeyboardButton(text="Chust")],
+        [KeyboardButton(text="Kasansoy"),KeyboardButton(text="Mingbuloq"),KeyboardButton(text="Namangan")],
+        [KeyboardButton(text="To'raqo'rg'on"),KeyboardButton(text="Norin"),KeyboardButton(text="Uchqo'rg'on")],
+        [KeyboardButton(text="Pop"),KeyboardButton(text="Yangiqo'rg'on"),KeyboardButton(text="Uychi")],
+        [KeyboardButton(text="Bosh sahifa"), KeyboardButton(text="Orqaga")],
+    ],
+    resize_keyboard=True,
+    # one_time_keyboard=True,
+)
+
+# ---- Navoidagi shaharlar
+navoi = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Zarafshon shahri"),KeyboardButton(text="Navoi shahri")],
+        [KeyboardButton(text="Karmana"),KeyboardButton(text="Konimex")],
+        [KeyboardButton(text="Navbahor"),KeyboardButton(text="Qiziltepa")],
+        [KeyboardButton(text="Nurota"),KeyboardButton(text="Tomdi")],
+        [KeyboardButton(text="Uchquduq"),KeyboardButton(text="Xatarchi")],
+        [KeyboardButton(text="Bosh sahifa"), KeyboardButton(text="Orqaga")],
+    ],
+)
+
+# --- Qashqadaryodagi shaharlar
+qoshqadaryo = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Mirishkor"),KeyboardButton(text="Chiroqchi"),KeyboardButton(text="Kitob")],
+        [KeyboardButton(text="Dehqonobod"),KeyboardButton(text="G'uzor"),KeyboardButton(text="Kasbi")],
+        [KeyboardButton(text="Kason"),KeyboardButton(text="Yakkabog'"),KeyboardButton(text="Muborak")],
+        [KeyboardButton(text="Nishon"),KeyboardButton(text="Qamashi"),KeyboardButton(text="Qarshi")],
+        [KeyboardButton(text="Qarshi shahar"),KeyboardButton(text="Shahrisabz shahri")],
+        [KeyboardButton(text="Bosh sahifa"), KeyboardButton(text="Orqaga")],
+    ],
+)
+
+
+""" ---- MEn Haydovchiman niki ---- """
+men_haydovchiman = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Andijon-Toshkent-Andijon")],
+        [KeyboardButton(text="Farg'ona-Toshkent-Farg'ona")],
+        [KeyboardButton(text="Buxoro-Toshkent-Buxoro")],
+        [KeyboardButton(text="Jizzax-Toshkent-Jizzax")],
+        [KeyboardButton(text="Namangan-Toshkent-Namangan")],
+        [KeyboardButton(text="Navoi-Toshkent-Navoi")],
+        [KeyboardButton(text="Qashqidaryo-Toshkent-Qashqidaryo")],
+        [KeyboardButton(text="Qoraqalpoqiston-Toshkent-Qoraqalpaqiston")],
+        [KeyboardButton(text="Samarqand-Toshkent-Samarqand")],
+        [KeyboardButton(text="Sirdayo-Toshkent-Sirdaryo")],
+        [KeyboardButton(text="Surxondaryo-Toshkent-Surxondaryo")],
+        [KeyboardButton(text="Xorazm-Toshkent-Xorazm")],
+        [KeyboardButton(text="Bosh sahifa"), KeyboardButton(text="Orqaga")],
+    ],
+    resize_keyboard=True,
+    # one_time_keyboard=True,
 )
